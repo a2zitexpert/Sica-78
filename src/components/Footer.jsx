eb01c10f927671ec3c9ffa4ce6f97bb5af7c1411
Rupesh <hello@sandrapascual.com>
2025-09-23 12:52:03 +0530

    Updated Footer, Header and index.css

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,8 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div>
-      
-    </div>
+      <p className='footer-front'> copyright ©2025 sicaschool78.org all rights reserved.</p></div>
   )
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const header = () => {
   return (
     <div>
-      
+      <h1 className='header-section'>SICA SCHOOL 78</h1>
     </div>
   )
 }
